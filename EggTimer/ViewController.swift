0255ad1f6788fe5a9a3100f93cb5cf8cca3585b3
Nawaf <Nawaf.jr9@hotmial.com>
2020-06-28 11:07:53 +0300

    Initial Commit

---
+++ EggTimer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EggTimer
+//
+//  Created by Nawaf B Al sharqi on 07/11/1441 AH.
+//  Copyright © 1441 Nawaf B Al sharqi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
